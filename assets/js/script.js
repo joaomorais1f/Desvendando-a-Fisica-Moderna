40e74309eab7109732318abe099b7f74bee9f354
joaomorais1f <ifsp.joaov@gmail.com>
2021-01-18 23:47:52 -0300

    fix answer's bug and points counter done

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,10 @@ const Questions = [
     },
     {
         "pergunta": 'pergunta teste 2',
-        "alternativa_A": "teste a",
-        "alternativa_B": "teste b",
-        "alternativa_C": "teste c",
-        "alternativa_D": "teste d",
+        "alternativa_A": "teste a 2",
+        "alternativa_B": "teste b 2",
+        "alternativa_C": "teste c 2 ",
+        "alternativa_D": "teste d 2",
         "resposta": "D",
         "explicacao": "por meio do enunciado 2..."       
     },
@@ -33,11 +33,13 @@ const Wrong = document.querySelector(".wrong-container")
 const CorrectContainer = document.querySelector(".correct-container")
 const NextQuestion = document.querySelector(".next-question")
 const AlternativesContainer = document.querySelector(".question-alternatives")
-
+const QuestionContainer = document.querySelector(".question-container")
+let Points = 0
 
 function StartGame() {
     AlternativesContainer.classList.remove("clicked")
     const CurrentQuizIndex = Questions[QuestionIndex]
+    const HeaderNumber = document.querySelector(".question-number").textContent = `Pergunta ${QuestionIndex + 1}`
     const Question = document.querySelector(".question").textContent = CurrentQuizIndex.pergunta
     for (let i = 0; i < Alternatives.length; i++) {
         Alternatives[i].value === "A" ? Alternatives[i].textContent = CurrentQuizIndex.alternativa_A : ''
@@ -45,23 +47,27 @@ function StartGame() {
         Alternatives[i].value === "C" ? Alternatives[i].textContent = CurrentQuizIndex.alternativa_C : ''
         Alternatives[i].value === "D" ? Alternatives[i].textContent = CurrentQuizIndex.alternativa_D : ''
         Alternatives[i].style.color = "black"
-        Alternatives[i].addEventListener("click", () => {
+        Alternatives[i].removeEventListener('click', Alternatives[i].event, false)
+        Alternatives[i].event = () => {
             NextQuestion.classList.add("active")
             if (!AlternativesContainer.classList.contains("clicked")) {
                 if (Alternatives[i].value === CurrentQuizIndex.resposta) {
                     CorrectContainer.classList.add("correct")
                     AlternativesContainer.classList.add("clicked")
+                    Points++
                 } else {
                     Wrong.classList.add("wrong")
                     Wrong.querySelector(".right-alternative").textContent = `Resposta ${CurrentQuizIndex.resposta}`
                     Wrong.querySelector(".explication-content").textContent = CurrentQuizIndex.explicacao
                     AlternativesContainer.classList.add("clicked")
                 }
+                AlternativesContainer.classList.add("clicked")
             } else {
-                    Alternatives[i].textContent = "É válido apenas uma tentativa!"
-                    Alternatives[i].style.color = "red"
+                Alternatives[i].textContent = "É válido apenas uma tentativa!"
+                Alternatives[i].style.color = "red"
             }
-        })
+        }
+        Alternatives[i].addEventListener("click", Alternatives[i].event, false)
     }
 }
 
@@ -71,6 +77,9 @@ NextQuestion.addEventListener("click", () => {
     Wrong.classList.remove("wrong")
     CorrectContainer.classList.remove("correct")
     NextQuestion.classList.remove("active")
+    // if (Questions.length - QuestionIndex == 0) {
+    //     NextQuestion.textContent = Points
+    // }
     StartGame()
 })
 
