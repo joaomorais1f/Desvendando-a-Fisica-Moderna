e65babb504a9a42487293ed350757f8f65311d01
joaomorais1f <ifsp.joaov@gmail.com>
2021-01-25 21:43:15 -0300

    adding some questions and fixing layout's bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,114 @@ const Questions = [
         "alternativa_D": "Raios beta, os quais têm a carga somente negativa. ",
         "resposta": "A",
         "explicacao": "A alternativa B está incorreta, pois os raios alfa são pouco penetrantes. Em relação à alternativa C, o erro está no fato de que os raios alfa tem uma estrutura composta por 2 prótons e 2 nêutrons. Já em relação à “D”, não podemos afirmar que raios beta possuem carga somente negativa, uma vez que emissões pósitrons também são possíveis com carga positiva (β+)."          
+    },
+    {
+        "pergunta": 'O experimento mental do Gato de Schrödinger teve como real motivação:',
+        "alternativa_A": "Contribuir com a área da Mecânica Quântica à medida em que seu experimento dava base para a interpretação de Copenhague.",
+        "alternativa_B": "Criticar a interpretação de Copenhague de forma irônica e hiperbólica, trazendo um estudo do mundo microscópico para o macroscópico.",
+        "alternativa_C": "Comprovar a existência de realidades paralelas, tendo em vista a dualidade existencial do gato: ora vivo, ora morto.",
+        "alternativa_D": "Refutar a Mecânica Clássica, uma vez que a teoria da mesma já não dava conta de explicar os fenômenos de partículas subatômicas.",
+        "resposta": "B",
+        "explicacao": "A alternativa A está incorreta, pois contribuir com a Mecânica Quântica foi uma consequência indireta do experimento, a real intenção era ironizar e derrubar a interpretação de Copenhague. É impossível ser a C, uma vez que o experimento não teve qualquer pretensão de comprovar a existência de realidades paralelas, e sim, tornar um absurdo a interpretação em questão. A alternativa D, também está incorreta, tendo em vista que refutar a Mecânica Clássica não estava nos planos de Schrödinger, até porque, no mundo macroscópico, suas leis se aplicam bem."
+    }, 
+    {
+        "pergunta": 'A Física Quântica, de fato é uma das áreas mais fascinantes da ciência, isso se deve principalmente pelo fato de ser misteriosa e trazer consigo concepções bem distintas das já consolidadas na humanidade. Um dos mistérios que ainda assolam a comunidade científica, com certeza é a do acontecido no Experimento da dupla fenda, sobre esse mistério, é correto afirmar: ',
+        "alternativa_A": "O grande mistério ainda vigente está na luz assumir comportamento ondulatório sem a observação de terceiros, e assumir comportamento corpuscular (partícula) com a observação de terceiros.",
+        "alternativa_B": "O mistério em questão se deve ao poder de curvatura da luz, uma vez que já foi comprovado a impossibilidade do fenômeno.",
+        "alternativa_C": "A incógnita deixada por esse experimento se traduz na contradição da natureza da luz: ora partícula, ora onda.",
+        "alternativa_D": "O enigma por trás, se deve ao fato do feixe de luz conseguir atravessar paredes espessas, fortalecendo a teoria do vazio entre núcleo atômico e elétrons.",
+        "resposta": "A",
+        "explicacao": "A alternativa B está incorreta pois além do mistério não ter a ver com a curvatura da luz, esse fenômeno é possível sim. Quanto a C, o erro está em assumir ser o mistério, a contradição da natureza da luz, tendo em vista que o experimento já foi feito com o prévio conhecimento disso, e não teve como consequência deixada a revelação do mesmo.Na realidade, a descoberta da dualidade da luz vem de antes com o embate de Newton e Huygens. A alternativa D, por sua vez, foge completamente do pretendido.Tendo em vista que o enigma proposto não tem relação alguma com a luz transpassar paredes."
+    }, 
+    {
+        "pergunta": "Assinale a alternativa que corresponde à validação das afirmações abaixo sobre a Física Moderna. V (Verdadeiro)  F (Falso): I. Albert Einstein teve grande participação no desenrolar da Física Moderna, limitando-se ao estudo da relatividade. \n II. Não existe, até agora, equação ou lei que relaciona uma partícula com onda. \n III. A Física Moderna foi e ainda é responsável pela exploração do mundo microscópico, bem como das grandes velocidades (próximas ou igual a da luz).",
+        "alternativa_A": "V,F,F",
+        "alternativa_B": "V,V,V",
+        "alternativa_C": "F,V,V",
+        "alternativa_D": "F,F,V",
+        "resposta": "D",
+        "explicacao": "A afirmação I está errada pelo uso da palavra ‘’limitando-se’’. Albert Einstein também foi uma figura importante no campo da Física Quântica. A afirmação II também está errada, pois na realidade, existe a equação de Broglie que relaciona um comportamento ondulatório com um corpuscular, representada por comprimento da onda= constante de planck / massa do corpo.velocidade do corpo."
+    },
+    {
+        "pergunta": "Inicialmente três teorias de comportamentos da resistividade(capacidade de um material resistir a passagem de carga) com relação a temperatura foram elaboradas: Quando se abaixa a temperatura os elétrons param de obstruir a trajetória da corrente até chegar ao 0 absoluto e conduzir a corrente perfeitamente; A resistividade diminui com a temperatura mas nunca fica nula devido impurezas no material; Reduzir a temperatura significa aumentar a resistividade pois os elétrons congelariam. No entanto, para alguns materiais nenhuma das hipóteses se aplicava.O Mercúrio foi o primeiro material descoberto como supercondutor.Sobre esse tipo de material é correto afirmar que:",
+        "alternativa_A": "Os materiais isolantes são os materiais que não são supercondutores, como o nome indica, sendo o oposto na verdade. Sendo assim, nem todos os materiais são supercondutores.",
+        "alternativa_B": "Atualmente os materiais supercondutores possuem uso em larga escala e são muito importantes para a manutenção de inúmeros setores industriais, sendo vendidos abundantemente inclusive para empresas de pequeno e médio porte.",
+        "alternativa_C": "O Mercúrio foi o primeiro material descoberto como supercondutor. A sua resistividade desce linearmente de acordo com a queda de temperatura. No entanto, a partir de certa temperatura a resistividade cai bruscamente para 0, o que é comum nos super condutores.",
+        "alternativa_D": "O que possibilita utilizar supercondutores mundanamente é o fato da temperatura necessária ser possível de se alcançar com medidas como ar condicionado e salas refrigeradas.",
+        "resposta": "C",
+        "explicacao": "A afirmativa A está incorreta pois inclusive materiais isolantes podem transmitir corrente no estado supercondutor, como a argila. A letra B e D estão incorretas,  pois os materiais no estado supercondutor são difíceis de conseguir, visto que é necessária uma temperatura muito baixa e condições muito específicas, não sendo possível utilizá-los facilmente. No entanto, os supercondutores já são utilizados em alguns setores como nas ressonâncias magnéticas A alternativa correta é a alternativa C."
+    },
+    {
+        "pergunta": "Sobre o conceito de meia vida é correto afirmar que: ",
+        "alternativa_A": "Não é possível saber especificamente quando o elemento radioativo vai decair, basicamente utiliza-se da estatística.",
+        "alternativa_B": "Meia vida significa o tempo necessário para que metade dos átomos de um elemento não radioativo decaiam.",
+        "alternativa_C": "É possível retardar ou acelerar o decaimento de um átomo através de aceleradores de partículas.",
+        "alternativa_D": "Após o decaimento do elemento, o material se extingue. ",
+        "resposta": "A",
+        "explicacao": "Não é possível saber especificamente quando o elemento radioativo vai decair, basicamente utiliza-se da estatística onde várias observações para se fazer uma média. A letra B está incorreta pois meia vida está totalmente relacionada a elementos radioativos e, além disso, não é possível alterar este tempo, como descrito na alternativa C. Por fim, após o decaimento do elemento ele geralmente se transforma em outro elemento até que atinja um elemento estável, não sumindo ou acabando, como na afirmação D."
+    },
+    {
+        "pergunta": "No decaimento gama de um elemento radioativo X, com 30 prótons, 40 nêutrons e 70 de massa, a massa final do elemento é calculada em: ",
+        "alternativa_A": "68",
+        "alternativa_B": "72",
+        "alternativa_C": "71",
+        "alternativa_D": "Nenhuma das alternativas anteriores.",
+        "resposta": "D",
+        "explicacao": "No caso de decaimentos gama o elemento não se altera pois ele é uma onda. Geralmente esse decaimento vem em conjunto com o decaimento alfa e beta (elementos que realmente perdem massa) pois sobram resíduos de energia a ser eliminada."
+    },
+    {
+        "pergunta": "Os materiais podem ser divididos em duas categorias com base na sua capacidade de conduzir eletricidade. Os condutores são os materiais com pouca resistividade e alta condutividade, já os isolantes, pouca condutividade e muita resistividade. Com base nessa afirmação e seus conhecimentos, responda: o que são supercondutores?",
+        "alternativa_A": "Materiais com alta condutividade à temperatura ambiente ao nível do mar.",
+        "alternativa_B": "Materiais com resistência elétrica quase nula à temperatura ambiente.",
+        "alternativa_C": "Materiais com resistência elétrica quase nula à baixas temperaturas.",
+        "alternativa_D": "Materiais com alta condutividade e baixa resistência a qualquer temperatura.",
+        "resposta": "C",
+        "explicacao": " Os materiais supercondutores conduzem corrente elétrica com resistência quase nula à baixas temperaturas.Um exemplo é o Mercúrio(Hg), o qual conduz corrente elétrica com resistividade quase nula à uma temperatura de 4º K.",
+    },
+    {
+        "pergunta": "No estudo da capacidade condução de corrente elétrica dos materiais, observa-se a existência dos materiais isolantes e condutores, no qual os da primeira classe possuem altas resistências e os da segunda, alta condutividade. Porém, há também os materiais que estão entre os isolantes e os condutores, os quais recebem o nome de semicondutores e são de grande importância no desenvolvimento de equipamentos de Tecnologia da Informação e Comunicação(TIC). Para a sua utilização, é necessário o processo de incluir impurezas na rede cristalina desses materiais no estado sólido. Esse processo recebe o nome de:",
+        "alternativa_A": "Enriquecimento",
+        "alternativa_B": "Dopagem",
+        "alternativa_C": "Centrifugação",
+        "alternativa_D": "Decantação",
+        "resposta": "B",
+        "explicacao": " O processo descrito é o processo de dopagem. a) O enriquecimento relaciona- se com o processo em que o  urânio cujo teor de 235U(urânio - 235) foi aumentado, através de um processo de separação de isótopos. c) Centrifugação é um método de separação de misturas heterogêneas de sólidos com líquidos ou somente de líquidos. d) Decantação é um processo físico de separação de misturas heterogêneas do tipo líquido - sólido e líquido - líquido "
+    },
+    {
+        "pergunta": "É evidente que muitos fenômenos físicos são controlados pela ciência e trazem diversos benefícios na área de medicina. Um exemplo disso é a tomografia por emissão de pósitrons. Na qual consiste em: ",
+        "alternativa_A": "Na utilização de emissão de raios X e, com base na absorção desses raios por partes específicas do corpo, visualizar componentes internos do corpo humano. ",
+        "alternativa_B": "Na utilização de emissão de raios X para visualizar compostos internos do corpo humano. Porém, diferente da radiografia, utiliza dos recursos computacionais para visualizar as imagens de diversos ângulos.",
+        "alternativa_C": "Na captura de imagens por meio dos pósitrons emitidos pelo corpo humano após ingerir uma substância radioativa. ",
+        "alternativa_D": "Todas as alternativas acima estão incorretas.",
+        "resposta": "C",
+        "explicacao": "a Tomografia por emissão de pósitrons consiste na captura de imagem de partes internas do corpo por meio dos pósitrons que são emitidos depois da ingestão de substância radioativa."
+    },
+    {
+        "pergunta": "A respeito dos riscos associados à radiação ionizante, selecione a alternativa falsa:",
+        "alternativa_A": "Ao passo em que os exames por imagem podem corroborar para salvar vidas, eles também podem ser prejudiciais à saúde.",
+        "alternativa_B": "Dependendo do grau de exposição, a radiação pode causar alterações no DNA.",
+        "alternativa_C": "A incidência da radiação em exames por imagens para gestantes é contraindicada pelas alterações físico-químicas intracelulares que podem causar ao organismo e, portanto, podem impactar, em alguns casos, negativamente para o desenvolvimento do feto. ",
+        "alternativa_D": "A radiação ionizante pode ser prejudicial à saúde, pois quando ocorre, é capaz de transferir energia suficiente para expulsar os prótons de sua órbita.",
+        "resposta": "D",
+        "explicacao": "O uso inadequado de um termo (prótons), altera toda explicação.Em teoria, para que seja possível resultar em íons, a radiação ionizante deveria ser aquela capaz de transferir energia suficiente para expulsar os elétrons de sua órbita "
+    },
+    {
+        "pergunta": "Assinale a alternativa que melhor descreve as propriedades de um radioisótopo:",
+        "alternativa_A": "Radioisótopos são nada menos que isótopos instáveis de um elemento que decaem ou se desintegram, emitindo assim,  radiação. Eles são frequentemente utilizados na medicina, agricultura e engenharia, mas pelo alto grau de periculosidade são executados por meio de robôs.",
+        "alternativa_B": "Radioisótopos é o nome dado aos isótopos (elementos de mesma massa) que emitem radiação.",
+        "alternativa_C": "Pode-se citar como exemplo de radioisótopos os seguintes elementos: Urânio, hidrogênio e fósforo. ",
+        "alternativa_D": "Os átomos de isótopos radioativos podem ser definidos por serem muito estáveis e desse modo, responsáveis por liberar muita energia pela liberação de partículas eletromagnéticas. ",
+        "resposta": "A",
+        "explicacao": "Radioisótopos são isótopos (elementos com o mesmo número atômico) instáveis, os quais são sujeitos ao processo de decaimento radioativo, estes possuem uma gama de aplicações na agricultura e indústria, colaborando também para a medicina em diversos diagnósticos."
+    },
+    {
+        "pergunta": " A respeito da realização dos exames por imagem, assinale a alternativa falsa. ",
+        "alternativa_A": "A radiografia é um exame por imagem que utiliza raios X. Ao decorrer do procedimento, um feixe heterogêneo de raios X é produzido por um gerador projetado sobre a parte do corpo a ser examinada. ",
+        "alternativa_B": "Ultrassom é um exame por imagem responsável por captar ondas eletromagnéticas por meio do contato com o corpo humano e um transdutor.",
+        "alternativa_C": "Ecocardiograma é um exame realizado a partir de um aparelho de ultrassom, o qual é responsável por captar ondas sonoras e transformá-las em imagem.",
+        "alternativa_D": "Tomografia é um exame realizado a partir dos mesmos princípios que os outros exames por meio de raio X, no entanto, o exame pode oferecer ainda mais detalhes, visto que o aparelho emite vários feixes ao mesmo tempo.",
+        "resposta": "B",
+        "explicacao": "O ultrassom é um exame por imagem responsável por captar ondas sonoras e transformá- las em imagens.O uso incorreto do termo “ondas eletromagnéticas” altera o conceito de sua realização."
     }
 ]
 
@@ -43,9 +151,11 @@ const HeaderContent = document.querySelector(".game-header-content")
 const IntroGame = document.querySelector(".intro-container")
 const StartGameButton = document.querySelector(".start-game")
 const PlayerName = document.getElementById("player")
+const RestartGame = document.querySelector(".restart-game")
 
 HeaderContainer.style.display = "none"
 IntroGame.classList.add("active")
+
 
 StartGameButton.addEventListener("click", () => {
     if (PlayerName.value.length < 3 || PlayerName.value.length > 30 ) {
@@ -58,6 +168,8 @@ StartGameButton.addEventListener("click", () => {
 })
 
 function StartGame() {
+
+    console.log(AlternativesContainer.clientHeight)
     AlternativesContainer.classList.remove("clicked")
     const CurrentQuizIndex = Questions[QuestionIndex]
     HeaderContent.textContent = `Pergunta ${QuestionIndex + 1}`
@@ -65,7 +177,7 @@ function StartGame() {
     for (let i = 0; i < Alternatives.length; i++) {
         Alternatives[i].value === "A" ? Alternatives[i].querySelector(".alternative-text-a").textContent = CurrentQuizIndex.alternativa_A : ''
         Alternatives[i].value === "B" ? Alternatives[i].querySelector(".alternative-text-b").textContent = CurrentQuizIndex.alternativa_B : ''
-        Alternatives[i].value === "C" ? Alternatives[i].querySelector(".alternative-text-c").innerText = CurrentQuizIndex.alternativa_C : ''
+        Alternatives[i].value === "C" ? Alternatives[i].querySelector(".alternative-text-c").textContent = CurrentQuizIndex.alternativa_C : ''
         Alternatives[i].value === "D" ? Alternatives[i].querySelector(".alternative-text-d").textContent = CurrentQuizIndex.alternativa_D : ''
         Alternatives[i].style.color = "black"
         Alternatives[i].removeEventListener('click', Alternatives[i].event, false)
@@ -89,8 +201,7 @@ function StartGame() {
                     Wrong.classList.add("wrong")
                     Wrong.querySelector(".right-alternative").textContent = `Resposta ${CurrentQuizIndex.resposta}`
                     Wrong.querySelector(".explication-content").textContent = CurrentQuizIndex.explicacao
-                    AlternativesContainer.classList.add("clicked")
-                    
+                    AlternativesContainer.classList.add("clicked")                    
                 }
                 AlternativesContainer.classList.add("clicked")
             } else {
@@ -105,7 +216,6 @@ NextQuestion.addEventListener("click", () => {
     QuestionIndex++
     Wrong.classList.remove("wrong")
     CorrectContainer.classList.remove("correct")
-    document.querySelector(".game-container").style.height = "610px"
     document.body.style.overflowY = "hidden"
     window.scrollTo(0, 0)
     NextQuestion.classList.remove("active") 
@@ -124,3 +234,9 @@ NextQuestion.addEventListener("click", () => {
 })
 
 StartGame()
+
+RestartGame.addEventListener("click", () => {
+    GameContainer.classList.add("play")
+    PlayerStatus.style.display = "none"
+    StartGame()
+})
